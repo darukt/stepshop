3d7e378affd33e5ba49fdcd8063ab3c6d0dba6a3
ITSTART <itstart@step.kz>
2025-05-15 18:25:59 +0500

    lesson_08: forms

--- authapp/views.py
+++ authapp/views.py
@@ -1,3 +1,34 @@
+from django.contrib import auth
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from authapp.forms import ShopUserLoginForm
+
+def login(request):
+    title = 'Вход'
+    if request.method == 'Post':
+        login_form = ShopUserLoginForm(data=request.POST)
+        if login_form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+
+            user = auth.authenticate(username=username, password=password)
+
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('mainapp:index'))
+    else:
+        login_form = ShopUserLoginForm()
+
+
+    context = {
+        'title': title,
+        'login_form': login_form,
+    }
+
+    return render(request, 'auth/login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('mainapp:index'))
